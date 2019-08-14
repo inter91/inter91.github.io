890fd9700314025d6461364905bbd885b8cb50bd
unknown <inter91@mail.ru>
2019-08-14 23:00:14 +0300

    14/08 22:50

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ checkbox.forEach(function(element){
 
 //cart
 
-const cart = document.getElementById('cart');
-const cartInner = document.querySelector('.cart');
-const cartClose = document.querySelector('.cart-close');
+const cart = document.getElementById('cart'),
+cartInner = document.querySelector('.cart'),
+cartClose = document.querySelector('.cart-close');
 
 
     cart.addEventListener('click',function(){
@@ -32,23 +32,46 @@ const cartClose = document.querySelector('.cart-close');
 
 //add to cart
 
-const cards = document.querySelectorAll('.goods .card');
-const cartWrapper = document.querySelector('.cart-wrapper');
-const cartEmpty = document.getElementById('cart-empty');
-const cartCounter = document.querySelector('.counter');
+const cards = document.querySelectorAll('.goods .card'),
+cartWrapper = document.querySelector('.cart-wrapper'),
+cartEmpty = document.getElementById('cart-empty'),
+cartCounter = document.querySelector('.counter');
+
 cards.forEach((card)=>{
     const btn = card.querySelector('button');
     btn.addEventListener('click', function(){
 const cardClone = card.cloneNode(true);
 cartWrapper.appendChild(cardClone);
-cartEmpty.remove();
 cardSum();
+
+const removeBtn = cardClone.querySelector('.btn');
+removeBtn.textContent = 'Удалить из корзины';
+removeBtn.addEventListener('click',()=>{
+cardClone.remove();
+cardSum();
+});
     });
 });
 
 function cardSum(){
-    const cardsCart = cartWrapper.querySelectorAll('.card');
+    const cardsCart = cartWrapper.querySelectorAll('.card'),
+    cardsPrice = cartWrapper.querySelectorAll('.card-price'),
+    cartTotal = document.querySelector('.cart-total span');
+    let sum = 0;
     cartCounter.textContent = cardsCart.length;
+
+    cardsPrice.forEach(function(cardPrice) {
+       let price = parseFloat(cardPrice.textContent);
+       sum = sum + price;
+    });
+    console.log(sum);
+    cartTotal.textContent = sum;
+
+    if(cardsCart.length){
+        cartEmpty.remove();
+    } else {
+        cartWrapper.appendChild(cartEmpty);
+    }
 }
 
 //end add to cart
