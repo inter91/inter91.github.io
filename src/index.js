335afc1404a0e6ef9cd1e4cf10051fac071b6433
unknown <inter91@mail.ru>
2019-08-15 20:41:21 +0300

    15/08 evening

--- src/index.js
+++ src/index.js
@@ -82,16 +82,51 @@ addCart();
 
 //фильтр акции
 
-function actionPage {
+function actionPage() {
     const cards = document.querySelectorAll('.goods .card'),
-    discountCheckbox = document.getElementById('discount-checkbox');
+    discountCheckbox = document.getElementById('discount-checkbox'),
+    min = document.getElementById('min'),
+    max = document.getElementById('max'),
+    search = document.querySelector('.search-wrapper_input'),
+    searchBtn = document.querySelector('.search-btn');
 
-    discountCheckbo.addEventListener('click',() => {
+    discountCheckbox.addEventListener('click',() => {
         cards.forEach((card) => {
-            
+            if(discountCheckbox.checked){
+                if(!card.querySelector('.card-sale')){
+                    card.parentNode.style.display = 'none';
+                }
+            } else {
+                card.parentNode.style.display = '';
+            }
         });
     });
+function filterPrice(){
+cards.forEach((card) => {
+    const cardPrice = card.querySelector('.card-price'),
+    price = parseFloat(cardPrice.textContent);
 
+    if((min.value && price < min.value) || (max.value && price > max.value)){
+        card.parentNode.style.display = 'none';
+    } else {
+        card.parentNode.style.display = '';
+    }
+});
+}
+    min.addEventListener('change',filterPrice);
+    max.addEventListener('change',filterPrice);
+
+    searchBtn.addEventListener('click',() => {
+    const searchText = new RegExp(search.value.trim(), 'i');
+    cards.forEach((card) =>{
+        const title = card.querySelector('.card-title');
+        if(!searchText.test(title.textContent)){
+            card.parentNode.style.display = 'none';
+        } else {
+            card.parentNode.style.display = '';
+        }
+    });
+    });
 }
 actionPage();
 //end фильтр акции
